6e5111ba4b147df972792eeec1ea974a0f26b261
immaxd <89379905+immaxd@users.noreply.github.com>
2021-08-23 14:36:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'Sến ghê!!',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy 😉',
+    btnYes: 'Vẫn cứ là thích anh ❤',
+    btnNo: 'Không, ai thèm :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh 😍',
+    btnReply: 'Gửi cho anh 💋',
+    reply: 'Yêu thì yêu mà không yêu thì yêu 😁',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘',
+    messDesc: 'Đừng buồn nữa nha bé con, anh sẽ luôn ở đây bên em 💖',
+    btnAccept: 'Dạaaaaaa 😜',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
